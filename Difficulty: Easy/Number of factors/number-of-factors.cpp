63c72032a2f5be67d151eb189a5754b9a9bd0f9a
Varsha <varshhhh6@gmail.com>
2025-05-29 10:50:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of factors/number-of-factors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int countFactors(int N) {
+        // code here
+        int count = 0;
+        for(int i = 1; i <= N; i++){
+            if(N % i == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
