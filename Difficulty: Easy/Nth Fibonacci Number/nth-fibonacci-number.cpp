fdccc13578f225efb24e4a771bbc4f10791598c3
Varsha <varshhhh6@gmail.com>
2025-05-29 10:27:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n) 
+    {
+    int f[n + 2]; 
+    f[0] = 0;
+    f[1] = 1;
+    for (int i = 2; i <= n; i++)
+        f[i] = f[i - 1] + f[i - 2];
+    return f[n];
+}
+
+};
