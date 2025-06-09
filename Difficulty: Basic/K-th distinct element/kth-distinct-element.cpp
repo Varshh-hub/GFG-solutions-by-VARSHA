47567c50eb3aec438e7e55fb39bd918c08374696
Varsha <varshhhh6@gmail.com>
2025-06-09 09:43:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/K-th distinct element/kth-distinct-element.cpp
+++ Difficulty: Basic/K-th distinct element/kth-distinct-element.cpp
@@ -3,12 +3,13 @@ class Solution {
     int KthDistinct(vector<int> nums, int k) {
         // Code here.
         unordered_map<int, int> m;
+        int n = nums.size();
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < n ; i++) {
             m[nums[i]]++;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < n; i++) {
             if (m[nums[i]] == 1) {
                 k--;
                 if (k == 0) return nums[i];
