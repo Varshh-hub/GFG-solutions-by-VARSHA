de67b883f26dff3e2caf15d804f394fdaaf36d75
Varsha <varshhhh6@gmail.com>
2025-06-09 11:05:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(vector<int>& arr) {
+        // Your code here
+        sort(arr.begin(), arr.end());
+    }
+};
