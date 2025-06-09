995697220eb1fa014091f9fb2afbc60417f6d439
Varsha <varshhhh6@gmail.com>
2025-06-09 11:03:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
+        sort(arr.begin(), arr.end());
+    }
+};
