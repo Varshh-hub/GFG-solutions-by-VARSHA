923adc00da1822678495d7ff483f17abcf01a0ac
Varsha <varshhhh6@gmail.com>
2025-06-10 09:27:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -5,13 +5,13 @@ class Solution {
         // code here
         int n = arr.size();
         for (int i = 1; i < n; i++) {
-            int max = arr[i];
+            int key = arr[i];
             int j = i - 1;
-            while (j >= 0 && arr[j] > max) {
+            while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j + 1] = max;
+            arr[j + 1] = key;
         }
     }
 };
