0ceb98929bf84c39a1abe09a3131eb7970536605
Varsha <varshhhh6@gmail.com>
2025-06-10 14:27:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to find element in sorted array
+    // arr: input array
+    // k: element to be searched
+    bool searchInSorted(vector<int>& arr, int k) {
+        int low = 0, high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) return true;
+            else if (arr[mid] < k) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+};
