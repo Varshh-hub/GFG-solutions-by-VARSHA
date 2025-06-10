08b299f845067f85beb2f569b7603664dd6e28d0
Varsha <varshhhh6@gmail.com>
2025-06-10 09:09:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -3,6 +3,15 @@ class Solution {
     // Please change the array in-place
     void insertionSort(vector<int>& arr) {
         // code here
-        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            int max = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > max) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = max;
+        }
     }
 };
